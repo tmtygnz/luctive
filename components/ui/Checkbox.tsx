5d300308230ae30f4e3b2fe8e4031302de26503b
Jostimian <jostimian@gmail.com>
2021-09-11 21:50:37 +0800

    Checkbox added

---
+++ components/ui/Checkbox.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Checkbox = () => {
+	return (
+		<div>
+			<label className="inline-flex items-center">
+				<input type="checkbox" className="form-checkbox"/>
+				<span className="ml-2">Option 2</span>
+			</label>
+		</div>
+	)
+}
