520c7184a98ea1469773f1372b78cefdd74382b6
Jostimian <jostimian@gmail.com>
2021-09-13 17:24:10 +0800

    Added normal html props to the custom button component

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,14 +1,20 @@
-import React, { Children } from "react";
+import React, {
+  Children,
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+} from "react";
 import { Spinner } from "./Spinner";
 
 const buttonTypes = {
   primary:
     "bg-primary-normal py-2 px-6 hover:bg-primary-hover active:bg-primary-focus",
-  secondary:
-    "bg-gray-300 py-2 px-6 hover:bg-gray-400 active:bg-gray-600",
+  secondary: "bg-gray-300 py-2 px-6 hover:bg-gray-400 active:bg-gray-600",
 };
 
-export type ButtonProps = {
+export type ButtonProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> & {
   IsLoading?: boolean;
   IsDisabled?: boolean;
   Type?: keyof typeof buttonTypes;
@@ -19,6 +25,7 @@ export const Button: React.FC<ButtonProps> = ({
   IsLoading,
   IsDisabled,
   Type = "secondary",
+  ...props
 }) => {
   console.log(Type);
   return (
@@ -26,13 +33,14 @@ export const Button: React.FC<ButtonProps> = ({
       className={`${buttonTypes[Type]} 
 			rounded-md shadow-md text-white flex items-center justify-center
 			${IsDisabled && "cursor-not-allowed"}`}
+			{...props}
     >
       <span className={IsLoading ? "opacity-0" : "items-center"}>
         {children}
       </span>
       {IsLoading ? (
         <span className="absolute">
-          <Spinner colorType={Type}/>
+          <Spinner colorType={Type} />
         </span>
       ) : null}
     </button>
