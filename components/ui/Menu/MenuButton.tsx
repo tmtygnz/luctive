c1c5948016abab5ee0f812a81acc74f216ebc329
Jostimian <jostimian@gmail.com>
2021-09-14 09:12:22 +0800

    Dropdown Fixed

--- components/ui/Menu/MenuButton.tsx
+++ components/ui/Menu/MenuButton.tsx
@@ -4,19 +4,20 @@ import { MenuItem } from "./MenuItem";
 import { Menu } from "./Menu";
 
 export type MenuButtonProps = {
-	label?: string,
-}
+  label?: string;
+  className?: string;
+};
 
-export const MenuButton:React.FC<MenuButtonProps> = ({children, label}) => {
+export const MenuButton: React.FC<MenuButtonProps> = ({
+  children,
+  label,
+  className,
+}) => {
   const [show, setShow] = useState(false);
   return (
-    <div>
-      <Button onClick={() => setShow(!show)}>
-        {label}
-      </Button>
-			{show ? (
-				{children}
-        ) : null}
+    <div className="ml-56">
+      <Button onClick={() => setShow(!show)}>{label}</Button>
+      {show ? <div>{children}</div> : null}
     </div>
   );
 };
--- components/ui/Menu/MenuItem.tsx
+++ components/ui/Menu/MenuItem.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export const MenuItem: React.FC<any> = ({ children }) => {
   return (
     <Link href="#">
-      <span className="p-1 hover:bg-primary-normal rounded flex items-center">
+      <span className="p-1 cursor-pointer hover:bg-primary-normal rounded flex items-center">
 				{children}
 			</span>
     </Link>
