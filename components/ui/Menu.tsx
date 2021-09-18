276e8d83100c3399ebbe3a36f6a06513b53e6910
Jostimian <jostimian@gmail.com>
2021-09-18 17:25:00 +0800

    Fixed Dropdown closing when clicking a dropdown item

--- components/ui/Menu.tsx
+++ components/ui/Menu.tsx
@@ -1,37 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Button } from "./Button";
-import Link from "next/link";  
+import Link from "next/link";
 
 export type MenuProps = {
   label: string;
 };
 
 export const Menu: React.FC<MenuProps> = ({ children, label }) => {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(true);
+
+  let container = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    console.log("test");
+    document.addEventListener("mousedown", clickedOutside);
+    return () => {
+      console.log("burh");
+      document.removeEventListener("mousedown", clickedOutside);
+    };
+  });
+
+  const clickedOutside = (event: MouseEvent) => {
+    console.log(typeof event);
+    let a = event.target;
+    if (!container.current?.contains((event.target) as Node) && container.current) {
+      console.log("false");
+      setShow(false);
+    }
+  };
+
   return (
-    <div className="drop inline-block relative" onBlur={() => setShow(false)}>
+    <div className="drop inline-block relative" ref={container}>
       <Button Type="ghost" onClick={() => setShow(!show)}>
         {label}
       </Button>
-      <ul
-        className={`drop-items absolute ${show ? "block" : "hidden"} 
-			 rounded border shadow-2xl transition duration-150 ease-in-out absolute`}
-      >
-        {children}
-      </ul>
+      {show && (
+        <div
+          className={`drop-items absolute 
+					rounded border shadow-2xl transition duration-150 ease-in-out`}
+        >
+          {children}
+        </div>
+      )}
     </div>
   );
 };
 
 export const MenuItem: React.FC<any> = ({ children }) => {
   return (
-    <li
+    <div
       className="hover:bg-ghost-hover cursor-pointer  rounded 
 			m-1 py-1 pr-20 pl-2 transition duration-150"
     >
-      <Link href="#">
-				{children}
-			</Link>
-    </li>
+      <Link href="#">{children}</Link>
+    </div>
   );
 };
