e4df7adf0f500b6728296b0b9048371b8b9bc219
Timothy Ganoza <jostimian@gmail.com>
2021-09-25 22:09:18 +0800

    Change hamburger from button to menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { Input } from './ui/Input'
-import { Menu } from './ui/Menu'
-import { NavLink } from './ui/NavLink'
-import {GiHamburgerMenu} from  'react-icons/gi'
-import { Button } from './ui/Button'
+import React from "react";
+import { Input } from "./ui/Input";
+import { Menu, MenuItem } from "./ui/Menu";
+import { NavLink } from "./ui/NavLink";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Button } from "./ui/Button";
 
 export const Header = () => {
-	return (
-		<div className="px-2 py-2 bg-yellow-normal align-middle w-screen flex">
-			<Button Type="yellow" className="mx-2"><GiHamburgerMenu/> </Button>
-			<Input inputSize="sm" list="autocomplete" type="text" className="w-80"/>
-		</div>
-	)
-}
+  return (
+    <div className="px-2 py-2 bg-yellow-normal align-middle w-screen flex">
+      <Menu label={<GiHamburgerMenu/>} className="mx-2 my-1">
+				<MenuItem>Test</MenuItem>
+			</Menu>
+      <Input inputSize="sm" list="autocomplete" type="text" className="w-80" />
+    </div>
+  );
+};
