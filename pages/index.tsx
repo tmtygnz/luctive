e5f84cfc29aa75828613d97a6a285fef37d1c237
Timothy Ganoza <jostimian@gmail.com>
2021-10-02 09:32:13 +0800

    Main content margin when drawer is open

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,18 @@
 import type { NextPage } from "next";
-import { useState } from "react";
 import { Drawer } from "../components/Drawer";
 import { Header } from "../components/Header";
+import { useDrawer } from "../context/DrawerContext";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  console.log();
-  const [open, setOpen] = useState(false);
+  const DrawerCon = useDrawer();
 
   return (
     <div className={`${styles.container} box-border `}>
       <Header />
       <div className="main flex">
-        <Drawer IsOpen={open}></Drawer>
-        <div className="m-10">hi</div>
+        <Drawer></Drawer>
+        <div className={`${DrawerCon ? "ml-80" : "ml-10"} mt-10`}>hi</div>
       </div>
     </div>
   );
