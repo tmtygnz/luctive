0f15f098d6691e6cefcadc2dd9f74f8c006ddb46
Timothy Ganoza <jostimian@gmail.com>
2021-10-22 21:12:42 +0800

    Open dashboard if cookie exist

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 import Link from "next/link";
 import React from "react";
+import { Dashboard } from "../components/Dashboard";
 import { Button } from "../components/ui/Button";
 import { useUserID } from "../context/AuthContext";
 import styles from "../styles/Home.module.css";
 
 const Home: React.FC = () => {
-	const {userID, updateUserID} = useUserID();
-	console.log(userID);
+  const { userID, updateUserID } = useUserID();
   return (
     <div className={`${styles.container} overflow-hidden`}>
-      <Button>
-        <Link href="/users/signup">test</Link>
-      </Button>
+      {userID ? (
+        <Dashboard />
+      ) : (
+        <Button>
+          <Link href="/users/signup">test</Link>
+        </Button>
+      )}
     </div>
   );
 };
